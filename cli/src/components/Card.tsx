605a41e91384cfb2d045b31de78cd75d080ab491
Almaz Tazeev <tyj5resd@gmail.com>
2023-10-12 11:49:49 +0300

    cards style

--- cli/src/components/Card.tsx
+++ cli/src/components/Card.tsx
@@ -2,20 +2,21 @@ import {FC} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {City} from "@/data/initialData";
+import styles from "@/styles/card.module.css";
 
 interface CardPropsI {
   city: City;
 }
 
 export const Card: FC<CardPropsI> = ({city}) => {
-  return <div>
+  return <div className={styles.card}>
     <Image 
       width={0}
       height={0}
       sizes="100vw"
       src={city.preview}
       alt={`preview ${city.name}`}
-      style={{width: '100%', height: 'auto'}}
+      style={{width: '100%', height: 'auto', borderRadius: '10px'}}
     />
     <Link href={`/city/${city.id}`}>{city.name}</Link>
   </div>
