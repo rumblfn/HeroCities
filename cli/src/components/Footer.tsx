ab75749050f5c91630764b4fd8746fed1014bd5d
Almaz Tazeev <78506458+aaalace@users.noreply.github.com>
2023-10-22 21:34:20 +0300

    Update Footer.tsx

--- cli/src/components/Footer.tsx
+++ cli/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {FC} from "react";
 export const Footer: FC = () => {
   return <footer className="footer">
     <div className="container">
-      <div style={{display: "flex", flexDirection: "column", gap: 6, lineHeight: "85%"}}>
+      <div style={{display: "flex", flexDirection: "column", gap: 6}}>
         <p>Октябрь 2023 - группа БПИ235</p>
         <p>Проект по дисциплине "Основам российской государственности".</p>
         <p>Авторы: Вечеровский Роберт, Тазеев Алмаз, Кабиров Роман, Валиахметов Шамиль, Михаил Лукашук.</p>
